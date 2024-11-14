78936edcdd69a29412e1f1af44b28cc629242424
Brian Solano <brian_solano110012@yahoo.com>
2024-11-13 22:09:31 -0600

    Updated cli typescript to improve cli functionality

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -253,8 +253,7 @@ class Cli {
         },
       ])
       .then((answers) => {
-        const rearWheel = new Wheel(parseInt(answers.rearWheelDiameter), answers.rearWheelBrand)
-        const frontWheel = new Wheel(parseInt(answers.frontWheelDiameter), answers.frontWheelBrand)
+        
         const motorbike = new Motorbike(
           Cli.generateVin(),
           answers.color,
@@ -263,7 +262,7 @@ class Cli {
           parseInt(answers.year),
           parseInt(answers.weight),
           parseInt(answers.topSpeed),
-          [frontWheel, rearWheel]          
+          []          
         );
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
         // TODO: push the motorbike to the vehicles array
